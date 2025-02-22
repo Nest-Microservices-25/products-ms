d69dedee59ead91f54d6e7e5e82010246430301d
Munoz Daniel <dmunoz@micracel.com.ar>
2025-02-22 19:34:02 -0300

    find by id

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import {
+  Injectable,
+  Logger,
+  NotFoundException,
+  OnModuleInit,
+} from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
@@ -35,8 +40,17 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: number) {
+    const product = await this.product.findFirst({
+      where: {
+        id,
+      },
+    });
+    if (product) {
+      return product;
+    } else {
+      throw new NotFoundException(`Product with ID ${id} not found`);
+    }
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
