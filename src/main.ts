4e18193f8533e6a2b3dad4ddeed20f30fd1c5650
Munoz Daniel <dmunoz@micracel.com.ar>
2025-02-22 21:06:55 -0300

    convertir backend a microservicio

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config/envs';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('Main');
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.port,
+      },
+    },
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -16,9 +25,9 @@ async function bootstrap() {
       },
     }),
   );
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen();
 
-  logger.log(`\n🚀  Server running at:`);
+  logger.log(`\n🚀 PRODUCST MICROSERVICE running at:`);
   logger.log(`📜  API Docs: \x1b[36mhttp://localhost:${envs.port}/api\x1b[0m`);
   logger.log(`🔗  API URL: \x1b[32mhttp://localhost:${envs.port}\x1b[0m\n`);
 }
